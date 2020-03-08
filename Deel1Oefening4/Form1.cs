b912bdb036a3219261aad8b5ade8d6f177eea8c9
mixxamm <maximjanssensj5@gmail.com>
2020-03-08 15:41:36 +0100

    Voeg verduidelijking toe

--- Deel1Oefening4/Form1.cs
+++ Deel1Oefening4/Form1.cs
@@ -28,8 +28,11 @@ namespace Deel1Oefening4
                 conn.Open();
                 SqlCommand cmd = conn.CreateCommand();
                 cmd.CommandText = query;
+                //Hieronder 2 verschillende manieren om met parameters te werken, deze doen beiden hetzelfde, alleen geeft manier 1 iets meer controle en is manier 2 korter.
+                // Manier 1:
                 cmd.Parameters.Add("@Voornaam", SqlDbType.NVarChar);
                 cmd.Parameters["@Voornaam"].Value = $"%{textBoxVoornaam.Text}%";
+                // Manier 2:
                 cmd.Parameters.AddWithValue("@Naam", $"%{textBoxNaam.Text}%");
                 SqlDataReader reader = cmd.ExecuteReader();
                 int nummer = 1;
